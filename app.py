62193e761b423c3f64ac24797ac55fa3d06c7727
Ben Tran <windwalker46@users.noreply.github.com>
2025-12-29 14:32:53 -0600

    small deployment fix

--- app.py
+++ app.py
@@ -264,6 +264,6 @@ def delete_exercise(exercise_id):
 if __name__ == "__main__":
     os.makedirs("instance", exist_ok=True)
     init_db()
-    app.run(debug=True)
-    #app.run(host="0.0.0.0", port=5000)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
